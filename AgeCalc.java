b3bfc63e0c1193c31ca392826e793f1c94265584
Kaustubha_Pundir <kaustubhapundir@gmail.com>
2021-05-27 11:12:46 +0530

    Update AgeCalc.java

--- AgeCalc.java
+++ AgeCalc.java
@@ -1,39 +1,28 @@
 import java.util.Scanner;
 public class AgeCalc {
     public static void main(String[] args){
+        Scanner scan = new Scanner();
         System.out.println("Date and input format-: dd/mm/yy");
         System.out.println("Enter your DOB ----------");
         System.out.print("Day = ");
-        Scanner Day = new Scanner(System.in);
-        var Day1 = Day.nextLine();
-        var D1 = Integer.parseInt(Day1);
+        var D1=scan.nextInt();
         
         System.out.print("Month = ");
-        Scanner Month = new Scanner(System.in);
-        var Month1 = Month.nextLine();
-        var M1 = Integer.parseInt(Month1);
+        var M1 = scan.nextInt();
         
         System.out.print("Year = ");
-        Scanner Year = new Scanner(System.in);
-        var Year1 = Year.nextLine();
-        var Y1 = Integer.parseInt(Year1);
+        var Y1 = scan.nextInt();
         
         System.out.println("Your Date of Birth = " + D1 + "/" + M1 + "/" + Y1);
         System.out.println("Enter current (or to) Date---------------");
         System.out.print("Day = ");
-        Scanner Day22 = new Scanner(System.in);
-        var Day2 = Day22.nextLine();
-        var D2 = Integer.parseInt(Day2);
+        var D2 = scan.nextInt();
 
         System.out.print("Month = ");
-        Scanner Month22 = new Scanner(System.in);
-        var Month2 = Month22.nextLine();
-        var M2 = Integer.parseInt(Month2);
+        var M2 = scan.nextInt();
         
         System.out.print("Year = ");
-        Scanner Year22 = new Scanner(System.in);
-        var Year2 = Year22.nextLine();
-        var Y2 = Integer.parseInt(Year2);
+        var Y2 = scan.nextInt();
 
         System.out.println("Date (to) Entered = " + D2 + "/" + M2 + "/" + Y2);
         var ly=Y2%4;
@@ -82,11 +71,6 @@ public class AgeCalc {
                 System.out.print("Year = "+((Y2-1)-Y1));
             }
         }
-    Day.close();
-    Day22.close();
-    Month.close();
-    Month22.close();
-    Year.close();
-    Year22.close();   
+    scan.close(); 
     }    
 }
